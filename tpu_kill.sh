c587922e6dc92561fe1de35256e6c70880c18821
Junbum Lee <jun@beomi.net>
2023-07-20 15:22:01 +0900

    tpu kill requires remove lockfile

--- tpu_kill.sh
+++ tpu_kill.sh
@@ -4,4 +4,4 @@ export ZONE='us-central2-b'
 
 echo "[local] Killing TPU"
 gcloud compute tpus tpu-vm ssh $TPU_NAME \
---zone $ZONE --worker=all --command "sudo fuser -k /dev/accel0"
+--zone $ZONE --worker=all --command "sudo fuser -k /dev/accel0; sudo rm /tmp/libtpu_lockfile"
