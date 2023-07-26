2132d69e7d4b846b0c5cd8160603d4d233ec0eea
Junbum Lee <jun@beomi.net>
2023-07-26 16:05:31 +0900

    att dropout works

--- llama2-ko-7b-ko-shuffled-full.sh
+++ llama2-ko-7b-ko-shuffled-full.sh
@@ -24,7 +24,7 @@ python -m EasyLM.models.llama.llama_train \
 --save_model_freq=2500 \
 --save_milestone_freq=10000 \
 --load_llama_config=7b \
---update_llama_config={"resid_pdrop": 0.1, "embd_pdrop": 0.1} \
+--update_llama_config='{\"attn_pdrop\": 0.1}' \
 --train_dataset.type='json' \
 --train_dataset.text_processor.fields='text' \
 --train_dataset.json_dataset.path=gs://kodataset/kor_shuffle_dataset.jsonl \
